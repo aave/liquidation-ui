20a250d9d092605f1230dd55b8dd3aae71aa5895
Alex <argeares97@gmail.com>
2020-03-13 21:30:48 +0200

    fix: tx confirmation

--- src/screens/LiquidationConfirmation/index.tsx
+++ src/screens/LiquidationConfirmation/index.tsx
@@ -4,25 +4,20 @@ import BigNumber from 'bignumber.js';
 import queryString from 'query-string';
 
 import { useLiquidationCallMutation } from '../../apollo/generated';
-import { useWeb3ProviderHook } from '../../web3-data-provider/hooks/web3-provider-hook';
+import { useWeb3Context } from '../../web3-data-provider';
 import TxConfirmationView from 'components/TxConfirmationView';
 
 export default function LiquidationConfirmation() {
   const [LiquidationCallMutation] = useLiquidationCallMutation();
   const location = useLocation();
   const { collateralReserve, reserveId } = useParams();
-
-  const defaultNetwork = 'mainnet';
-  const [{ web3Context }] = useWeb3ProviderHook(defaultNetwork);
-  const { wallet } = web3Context;
+  const { wallet } = useWeb3Context();
 
   let amount: any = '';
   const query = queryString.parse(location.search);
   if (typeof query.amount === 'string') {
     amount = new BigNumber(query.amount);
   }
-
-  console.log(wallet);
 
   const liquidationCall = async () => {
     const result = await LiquidationCallMutation({
